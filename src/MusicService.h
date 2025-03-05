0248da60826ed3263b96f2c65957f6544bb335c3
octtoberrust <octtoberrust@outlook.com>
2025-03-04 21:42:47 -0600

    music service header

--- src/MusicService.h
+++ src/MusicService.h
@@ -1,1 +1,6 @@
- 
+class MusicService {
+public:
+    virtual bool authenticate(const std::string &token) = 0;
+    virtual std::string search(const std::string &query) = 0;
+    virtual ~MusicService() = default;
+}
