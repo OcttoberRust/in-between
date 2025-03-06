4e3da20b12242458d9b7eb79084487756f60a049
octtoberrust <octtoberrust@outlook.com>
2025-03-05 20:53:46 -0600

    commented description in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
- 
+#include <iostream>
+
+int main() {
+
+    //user selects music services to transfer playlists between
+    //authenticate
+    //retrieve user's playlist from source service
+    //query list of songs onto destination service and create playlist
+
+    return 0;
+}
